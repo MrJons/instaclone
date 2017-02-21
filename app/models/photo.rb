4bdeb7382d73192a1a8ef8491830dd65713d80ff
Jon Shad <shad_stick@hotmail.co.uk>
2017-02-21 17:16:06 +0000

    model validation test for photo additions

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
   has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :image, presence: true
 end
