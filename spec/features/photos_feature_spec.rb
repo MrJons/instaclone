5c4724a705a833fe2830c35398e6bfa12faa017b
Jon Shad <shad_stick@hotmail.co.uk>
2017-02-21 14:24:10 +0000

    first commit. Project set up and initial tests.

---
+++ spec/features/photos_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'photos' do
+
+  context 'no photos have been added' do
+    scenario 'A message to promp uploads should be shown' do
+      visit '/photos'
+      expect(page).to have_content("There's no photos... add one?")
+    end
+
+    scenario 'A link to add photos should be visible' do
+      visit '/photos'
+      expect(page).to have_link('New post')
+    end
+  end
+end
