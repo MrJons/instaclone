bc51c1014c771a53d14f538386dac1ef51dbd985
Jon Shad <shad_stick@hotmail.co.uk>
2017-02-23 17:37:05 +0000

    full feature test suite for log in/out and sign up. tests passing.

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -14,33 +14,41 @@ feature 'users' do
       expect(page).not_to have_link('sign out')
     end
 
-    # scenario 'can sign up/in' do
-    #   visit '/photos'
-    #   sign_up
-    #   expect(page).to have_current_path('/photos')
-    #   expect(page).to have_content('Signed up successfully.')
-    # end
+    scenario 'can sign up' do
+      visit '/photos'
+      sign_up
+      expect(page).to have_current_path('/')
+      expect(page).to have_content('You have signed up successfully.')
+    end
+
+    user_seed
+
+    scenario 'can sign in' do
+      visit '/photos'
+      sign_in
+      expect(page).to have_current_path('/')
+      expect(page).to have_content('Signed in successfully.')
+    end
   end
-  #
-  # context 'when signed in' do
-  #   before :each do
-  #     visit '/photos'
-  #     sign_up
-  #   end
-  #
-  #   scenario 'can see sign out link' do
-  #     puts current_user
-  #     expect(page).to have_link('sign out')
-  #   end
-  #
-  #   scenario 'cannot see sign up/in links' do
-  #     expect(page).not_to have_link('sign up')
-  #     expect(page).not_to have_link('sign in')
-  #   end
-  #
-  #   # scenario 'can sign out' do
-  #   #   click_link 'sign out'
-  #   #
-  #   # end
-  # end
+
+  context 'when signed in' do
+    before :each do
+      visit '/photos'
+      sign_up
+    end
+
+    scenario 'can see sign out link' do
+      expect(page).to have_link('sign out')
+    end
+
+    scenario 'cannot see sign up/in links' do
+      expect(page).not_to have_link('sign up')
+      expect(page).not_to have_link('sign in')
+    end
+
+    scenario 'can sign out' do
+      click_link 'sign out'
+      expect(page).to have_content('Signed out successfully.')
+    end
+  end
 end
--- spec/helper.rb
+++ spec/helper.rb
@@ -7,3 +7,22 @@ def new_post
   fill_in 'Description', with: 'first photo'
   click_button 'post'
 end
+
+def sign_up
+  click_link 'sign up'
+  fill_in 'Email', with: 'test@test.com'
+  fill_in 'Password', with: '12345678'
+  fill_in 'Password confirmation', with: '12345678'
+  click_button 'Sign up'
+end
+
+def user_seed
+  before {User.create(email: 'test1@test.com', password: '12345678')}
+end
+
+def sign_in
+  click_link 'sign in'
+  fill_in 'Email', with: 'test1@test.com'
+  fill_in 'Password', with: '12345678'
+  click_button 'Log in'
+end
